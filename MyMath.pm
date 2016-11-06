325cd7c6867957d9fb3a48c6c1df16340d6226ba
jon <jon@puffy.home>
2016-11-06 05:16:15 +0000

    Update

--- MyMath.pm
+++ MyMath.pm
@@ -4,29 +4,8 @@ print "Content-Type: text/html\n\n";
 use strict;
 use warnings;
 use Exporter qw (import);
-our @Export_ok = qw (nthprime listprimesupto);
+our @Export_ok = qw (listprimesupto);
 package MyMath;
-
-sub nthprime{
-   my $x = 5;
-   my $y;
-   my $factor;
-   my @primes = (2,3);
-
-   while($#primes +2 < $_[0]){
-      $factor = 3;
-      until ($x % $factor == 0){
-         $y = sqrt($x);
-         if ($y < $factor){
-               push(@primes, $x);
-               $factor = $x -2;
-         }
-         $factor += 2;
-      }
-      $x += 2;
-   }
-   return $primes[-1];
-}
 
 sub listprimesupto{
    my $x = 5;
